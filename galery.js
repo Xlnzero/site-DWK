1c9195bf848d3bfb8b6e3fbd9868b7a8dc0d06e6
Xlnzero <62247456+Xlnzero@users.noreply.github.com>
2022-04-08 18:29:23 +0300

    sdr

---
+++ galery.js
@@ -0,0 +1,96 @@
+function funclose(){$('.g_gal_bol').css('display','none');document.body.style.overflow = 'auto'}
+	
+function funnext1(){$('.g_gal_bol1').css('display','none');$('.g_gal_bol2').css('display','flex');}
+function funnext2(){$('.g_gal_bol2').css('display','none');$('.g_gal_bol3').css('display','flex');}
+function funnext3(){$('.g_gal_bol3').css('display','none');$('.g_gal_bol4').css('display','flex');}
+function funnext4(){$('.g_gal_bol4').css('display','none');$('.g_gal_bol5').css('display','flex');}
+function funnext5(){$('.g_gal_bol5').css('display','none');$('.g_gal_bol6').css('display','flex');}
+function funnext6(){$('.g_gal_bol6').css('display','none');$('.g_gal_bol7').css('display','flex');}
+function funnext7(){$('.g_gal_bol7').css('display','none');$('.g_gal_bol8').css('display','flex');}
+function funnext8(){$('.g_gal_bol8').css('display','none');$('.g_gal_bol9').css('display','flex');}
+function funnext9(){$('.g_gal_bol9').css('display','none');$('.g_gal_bol10').css('display','flex');}
+function funnext10(){$('.g_gal_bol10').css('display','none');$('.g_gal_bol11').css('display','flex');}
+function funnext11(){$('.g_gal_bol11').css('display','none');$('.g_gal_bol12').css('display','flex');}
+function funnext12(){$('.g_gal_bol12').css('display','none');$('.g_gal_bol13').css('display','flex');$('.nvid1').css('display','none');$('.nvid2').css('display','block');$('.nvid').css('display','grid');}
+	/*<!--Раскрываю весь список фотографий-->*/
+function funnext13(){$('.g_gal_bol13').css('display','none');$('.g_gal_bol14').css('display','flex');}
+function funnext14(){$('.g_gal_bol14').css('display','none');$('.g_gal_bol15').css('display','flex');}
+function funnext15(){$('.g_gal_bol15').css('display','none');$('.g_gal_bol16').css('display','flex');}
+function funnext16(){$('.g_gal_bol16').css('display','none');$('.g_gal_bol17').css('display','flex');}
+function funnext17(){$('.g_gal_bol17').css('display','none');$('.g_gal_bol18').css('display','flex');}
+function funnext18(){$('.g_gal_bol18').css('display','none');$('.g_gal_bol19').css('display','flex');}
+function funnext19(){$('.g_gal_bol19').css('display','none');$('.g_gal_bol20').css('display','flex');}
+function funnext20(){$('.g_gal_bol20').css('display','none');$('.g_gal_bol21').css('display','flex');}
+function funnext21(){$('.g_gal_bol21').css('display','none');$('.g_gal_bol22').css('display','flex');}
+function funnext22(){$('.g_gal_bol22').css('display','none');$('.g_gal_bol23').css('display','flex');}
+function funnext23(){$('.g_gal_bol23').css('display','none');$('.g_gal_bol24').css('display','flex');}
+function funnext24(){$('.g_gal_bol24').css('display','none');$('.g_gal_bol25').css('display','flex');}
+function funnext25(){$('.g_gal_bol25').css('display','none');$('.g_gal_bol26').css('display','flex');}
+function funnext26(){$('.g_gal_bol26').css('display','none');$('.g_gal_bol27').css('display','flex');}
+function funnext27(){$('.g_gal_bol27').css('display','none');$('.g_gal_bol28').css('display','flex');}
+function funnext28(){$('.g_gal_bol52').css('display','none');}
+
+	
+function funprev2(){$('.g_gal_bol2').css('display','none');$('.g_gal_bol1').css('display','flex');}
+function funprev3(){$('.g_gal_bol3').css('display','none');$('.g_gal_bol2').css('display','flex');}
+function funprev4(){$('.g_gal_bol4').css('display','none');$('.g_gal_bol3').css('display','flex');}
+function funprev5(){$('.g_gal_bol5').css('display','none');$('.g_gal_bol4').css('display','flex');}
+function funprev6(){$('.g_gal_bol6').css('display','none');$('.g_gal_bol5').css('display','flex');}
+function funprev7(){$('.g_gal_bol7').css('display','none');$('.g_gal_bol6').css('display','flex');}
+function funprev8(){$('.g_gal_bol8').css('display','none');$('.g_gal_bol7').css('display','flex');}
+function funprev9(){$('.g_gal_bol9').css('display','none');$('.g_gal_bol8').css('display','flex');}
+function funprev10(){$('.g_gal_bol10').css('display','none');$('.g_gal_bol9').css('display','flex');}
+function funprev11(){$('.g_gal_bol11').css('display','none');$('.g_gal_bol10').css('display','flex');}
+function funprev12(){$('.g_gal_bol12').css('display','none');$('.g_gal_bol11').css('display','flex');}
+function funprev13(){$('.g_gal_bol13').css('display','none');$('.g_gal_bol12').css('display','flex');$('.nvid1').css('display','block');$('.nvid2').css('display','none');$('.nvid').css('display','none');}
+	/*<!--Скрываю часть списка фотографий-->*/
+function funprev14(){$('.g_gal_bol14').css('display','none');$('.g_gal_bol13').css('display','flex');}
+function funprev15(){$('.g_gal_bol15').css('display','none');$('.g_gal_bol14').css('display','flex');}
+function funprev16(){$('.g_gal_bol16').css('display','none');$('.g_gal_bol15').css('display','flex');}
+function funprev17(){$('.g_gal_bol17').css('display','none');$('.g_gal_bol16').css('display','flex');}
+function funprev18(){$('.g_gal_bol18').css('display','none');$('.g_gal_bol17').css('display','flex');}
+function funprev19(){$('.g_gal_bol19').css('display','none');$('.g_gal_bol18').css('display','flex');}
+function funprev20(){$('.g_gal_bol20').css('display','none');$('.g_gal_bol19').css('display','flex');}
+function funprev21(){$('.g_gal_bol21').css('display','none');$('.g_gal_bol20').css('display','flex');}
+function funprev22(){$('.g_gal_bol22').css('display','none');$('.g_gal_bol21').css('display','flex');}
+function funprev23(){$('.g_gal_bol23').css('display','none');$('.g_gal_bol22').css('display','flex');}
+function funprev24(){$('.g_gal_bol24').css('display','none');$('.g_gal_bol23').css('display','flex');}
+function funprev25(){$('.g_gal_bol25').css('display','none');$('.g_gal_bol24').css('display','flex');}
+function funprev26(){$('.g_gal_bol26').css('display','none');$('.g_gal_bol25').css('display','flex');}
+function funprev27(){$('.g_gal_bol27').css('display','none');$('.g_gal_bol26').css('display','flex');}
+function funprev28(){$('.g_gal_bol28').css('display','none');$('.g_gal_bol27').css('display','flex');}
+
+	
+function fun1(){$('.g_gal_bol1').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun2(){$('.g_gal_bol2').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun3(){$('.g_gal_bol3').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun4(){$('.g_gal_bol4').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun5(){$('.g_gal_bol5').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun6(){$('.g_gal_bol6').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun7(){$('.g_gal_bol7').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun8(){$('.g_gal_bol8').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun9(){$('.g_gal_bol9').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun10(){$('.g_gal_bol10').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun11(){$('.g_gal_bol11').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun12(){$('.g_gal_bol12').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun13(){$('.g_gal_bol13').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun14(){$('.g_gal_bol14').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun15(){$('.g_gal_bol15').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun16(){$('.g_gal_bol16').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun17(){$('.g_gal_bol17').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun18(){$('.g_gal_bol18').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun19(){$('.g_gal_bol19').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun20(){$('.g_gal_bol20').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun21(){$('.g_gal_bol21').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun22(){$('.g_gal_bol22').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun23(){$('.g_gal_bol23').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun24(){$('.g_gal_bol24').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun25(){$('.g_gal_bol25').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun26(){$('.g_gal_bol26').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun27(){$('.g_gal_bol27').css('display','flex');document.body.style.overflow = 'hidden'}
+function fun28(){$('.g_gal_bol28').css('display','flex');document.body.style.overflow = 'hidden'}
+
+	
+function inv2() {$('.nvid1').css('display','none');$('.nvid2').css('display','block');$('.nvid').css('display','grid');}/*<!--Кнопка для разворачивания-->*/
+	
+function inv(){$('.nvid1').css('display','block');$('.nvid2').css('display','none');$('.nvid').css('display','none');}	/*<!--Кнопка для сворачивания-->*/
